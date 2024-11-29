f61e557bf1a222b2bf70b6ff5f2fc02b7369589c
MoathAlzoughah <moath.alzoughah@gmail.com>
2024-11-29 17:09:05 +0300

    New update

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,29 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+
+    baseUrl:"https://magento.softwaretestingboard.com/",
+    // viewportHeight: 800,
+    // viewportWidth: 800,
+    // watchForFileChanges:true,
+    video:false,
+    videosFolder:"My Videos Folder",
+    videoCompression:0,
+    
+    reporter: "mochawesome",
+      reporterOptions: {
+        reportDir: "cypress/myReport", // where to save in directory
+        overwrite: false,
+        html: true,
+        json: false,
+        timestamp: "mmddyyyy_HHMMss",
+    }
+
+
+    
   },
+
+
+
+
 });
