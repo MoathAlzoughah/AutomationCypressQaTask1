df6493f5fc2afea8a77000d893c3f297e3c4c23d
MoathAlzoughah <moath.alzoughah@gmail.com>
2024-12-13 23:22:17 +0300

    new

---
+++ cypress/e2e/cucumber/Tests/CypressTask3/CypressTask3.cy.js
@@ -0,0 +1,49 @@
+import { Given , When , Then } from "cypress-cucumber-preprocessor/steps";
+
+Given("Navigate on the homepage",() => {
+
+cy.visit("/");
+
+})
+
+
+When("Navigate to the Whats New page",() => {
+
+cy.get('nav')
+
+cy.contains("What's New").click();
+
+})
+
+
+Then("Should see a list of all categories displayed in the section",() => {
+
+
+cy.get('.categories-menu li').should('be.visible');
+    
+})
+
+
+Then("Print the names of these categories", () => {
+       
+cy.get('.categories-menu li').then((categories) => {
+            
+    for (let i = 0; i < 6; ) {categories[12]
+        cy.wrap(categories[i++]).invoke('text').then((categoryName) => {
+        cy.log(categoryName);
+
+     });
+
+  }
+
+ });
+
+
+});
+
+        
+        
+    
+
+
+
